e0da5094d463215bf8f6aaad97eb771123792664
corvuscorae <corvuscorae@gmail.com>
2024-06-06 16:57:15 -0700

    randomized tiles for dungeon construction

--- src/Scenes/LEVEL_1.js
+++ src/Scenes/LEVEL_1.js
@@ -8,7 +8,8 @@ class LEVEL_1 extends Phaser.Scene {
     init() {
         this.SPEED = 500; 
         this.TILESIZE = 32;
-        this.ROOMSIZE = 25;
+        this.WALLSIZE = 25;
+        this.DOORSIZE = 2;
 
         this.endScene = false;
 
@@ -64,22 +65,17 @@ class LEVEL_1 extends Phaser.Scene {
         // https://itnext.io/modular-game-worlds-in-phaser-3-tilemaps-3-procedural-dungeon-3bc19b841cd //
         this.dungeon = new Dungeon({
             // The dungeon's grid size
-            width: this.ROOMSIZE*5,
-            height: this.ROOMSIZE*5,
-            doorPadding: 2,
-            doorSize: 2,
+            width: this.WALLSIZE*5,
+            height: this.WALLSIZE*5,
+            doorPadding: this.DOORSIZE*2,
+            doorSize: this.DOORSIZE,
             rooms: {
-                width: { min: this.ROOMSIZE, max: this.ROOMSIZE },
-                height: { min: this.ROOMSIZE, max: this.ROOMSIZE },
-                maxArea: Math.pow(this.ROOMSIZE, 2),
+                width: { min: this.WALLSIZE, max: this.WALLSIZE },
+                height: { min: this.WALLSIZE, max: this.WALLSIZE },
+                maxArea: Math.pow(this.WALLSIZE, 2),
                 maxRooms: 20
             }
             });
-        
-        
-        for(let room of this.dungeon.rooms){
-            room.discovered = false;
-        }
         
         // Create a blank map
         const map = this.make.tilemap({
@@ -93,19 +89,134 @@ class LEVEL_1 extends Phaser.Scene {
         this.dungeonTile = map.addTilesetImage("dungeon-packed", "dungeon");
 
         // Create an empty layer and give it the name "Layer 1"
+        this.BGLayer = map.createBlankLayer("background", this.dungeonTile);
         this.groundLayer = map.createBlankLayer("ground", this.dungeonTile);
         this.catLayer = map.createBlankLayer("stuff", this.dungeonTile);
 
-        // Turn the dungeon into a 2D array of tiles where each of the four types of tiles is mapped to a
-        // tile index within our tileset. Note: using -1 for empty tiles means they won't render.
-        let emptyID = -1;   let floorID = 100;
-        let doorID = 93;    let wallID = 5
-        const mappedTiles = this.dungeon.getMappedTiles({ 
-            empty: emptyID, floor: floorID, door: doorID, wall: wallID });
+        // tile indexes
+        let empty = -1;   
+        let bg = 100;   
+        let normalFloors = [69,70,71,72,85,88,104,120]; 
+        let brokenFloors = [86,87,101,102,103,117,118,119];
+        let doors = {
+            left: [84,177],      // top,  bottom
+            right: [82, 176],    // top,  bottom
+            top: [126,127],       // left, right
+            bottom: [160,161]     // left, right
+        }; 
+        let doorWrap = {
+            left: [empty, 54],  // top,  bottom
+            right: [empty, 53], // top,  bottom
+            top: [84,82],       // left, right
+            bottom: [54,53]     // left, right
+        }     
+        let walls = {
+            left: 20,
+            right: 25,
+            top: [5,6,7,8],
+            bottom: [37,38,39,40],
+            corner: [4,9,36,41],  // topleft, topright, bottomleft, bottomright
+            misc: [84,82,53,54]
+        };
 
-        // Drop a 2D array into the map at (0, 0)
-        this.groundLayer.putTilesAt(mappedTiles, 0, 0);
-        /////////////////////////////////////////////////////////////////////////////////////////////////
+        // Set all tiles in the ground layer with blank tiles (brown tile)
+        this.BGLayer.fill(bg);
+        
+        //*************************************************************************************//
+        // TODO: FIGURE OUT HOW TO MAKE ALLADIS A SEPARATE CLASS
+        //********************************// BUILD-A-DUNGEON //********************************//
+        for(let room of this.dungeon.rooms){
+            room.discovered = false;
+            // Fill the room (minus the walls) with mostly clean floor tiles (90% of the time), but
+            // occasionally place a dirty tile (10% of the time).
+            this.groundLayer.weightedRandomize(
+                [{ index: normalFloors, weight: 9 },    // 9/10 times, use index 6
+                { index: brokenFloors, weight: 1 }],    // 1/10 times, randomly pick 7, 8 or 26
+                room.x + 1, room.y + 1, 
+                room.width - 2, room.height - 2);
+
+                // Place the room corners tiles
+                this.groundLayer.putTileAt(walls.corner[0], room.left,  room.top);
+                this.groundLayer.putTileAt(walls.corner[1], room.right, room.top);
+                this.groundLayer.putTileAt(walls.corner[2], room.left,  room.bottom);
+                this.groundLayer.putTileAt(walls.corner[3], room.right, room.bottom);
+
+                // place left and right walls
+                this.groundLayer.fill(walls.left, room.left,  room.top + 1, 1, room.height - 2); // Left
+                this.groundLayer.fill(walls.right, room.right, room.top + 1, 1, room.height - 2); // Right
+
+                // top wall
+                map.randomize(
+                    room.x + 1, room.y, 
+                    room.width - 2, 1,
+                    walls.top, this.groundLayer
+                );
+
+                // bottom wall
+                map.randomize(
+                    room.x + 1, room.y + room.height - 1, 
+                    room.width - 2, 1,
+                    walls.bottom, this.groundLayer
+                );
+
+                // doors
+                let roomDoor = room.getDoorLocations();
+                let s = this.DOORSIZE;
+                for(let i = 0; i < roomDoor.length; i+=s){
+                    //console.log(roomDoor[i]);
+                    // LEFT
+                    if(roomDoor[i].x === 0){    
+                        // top edge
+                        this.groundLayer.putTileAt(doors.left[0], 
+                            room.x + roomDoor[i].x, room.y + roomDoor[i].y - 1);
+                        // bottom edge
+                        this.groundLayer.putTileAt(doors.left[doors.left.length-1], 
+                            room.x + roomDoor[i+s-1].x, room.y + roomDoor[i+s-1].y - 1);
+                        // door wrap
+                        this.groundLayer.putTileAt(doorWrap.left[1], 
+                            room.x + roomDoor[i+s-1].x, room.y + roomDoor[i+s-1].y);
+                    // RIGHT
+                    } else if(roomDoor[i].x === room.width - 1){    
+                        // top edge
+                        this.groundLayer.putTileAt(doors.right[0], 
+                            room.x + roomDoor[i].x, room.y + roomDoor[i].y - 1);
+                        // bottom edge
+                        this.groundLayer.putTileAt(doors.right[doors.right.length-1], 
+                            room.x + roomDoor[i+s-1].x, room.y + roomDoor[i+s-1].y - 1);
+                        // door wrap
+                        this.groundLayer.putTileAt(doorWrap.right[1], 
+                            room.x + roomDoor[i+s-1].x, room.y + roomDoor[i+s-1].y);
+                    // TOP
+                    } else if(roomDoor[i].y === 0){                 
+                        // left edge
+                        this.groundLayer.putTileAt(doors.top[0], 
+                            room.x + roomDoor[i].x - 1, room.y + roomDoor[i].y);
+                        // right edge
+                        this.groundLayer.putTileAt(doors.top[doors.top.length-1], 
+                            room.x + roomDoor[i+s-1].x - 1, room.y + roomDoor[i+s-1].y);
+                        // door wrap
+                        this.groundLayer.putTileAt(doorWrap.top[0], 
+                            room.x + roomDoor[i].x - 2, room.y + roomDoor[i+s-1].y);
+                        this.groundLayer.putTileAt(doorWrap.top[1], 
+                            room.x + roomDoor[i+s-1].x, room.y + roomDoor[i+s-1].y);
+                    // BOTTOM
+                    } else if(roomDoor[i].y === room.height - 1){   
+                        // left edge
+                        this.groundLayer.putTileAt(doors.bottom[0], 
+                            room.x + roomDoor[i].x - 1, room.y + roomDoor[i].y);
+                        // right edge
+                        this.groundLayer.putTileAt(doors.bottom[doors.bottom.length-1], 
+                            room.x + roomDoor[i+s-1].x - 1, room.y + roomDoor[i+s-1].y);
+                        // door wrap
+                        this.groundLayer.putTileAt(doorWrap.bottom[0], 
+                            room.x + roomDoor[i].x - 2, room.y + roomDoor[i+s-1].y);
+                        this.groundLayer.putTileAt(doorWrap.bottom[1], 
+                            room.x + roomDoor[i+s-1].x, room.y + roomDoor[i+s-1].y);
+                    }
+                    
+                }
+        }
+        //*************************************************************************************//
         
         //* PLAYER *//
         this.guy = new Player(this,
@@ -117,11 +228,11 @@ class LEVEL_1 extends Phaser.Scene {
             .setOffset(this.guy.width / 4, this.guy.height / 2);
             
         // make walls collidable 
-        this.groundLayer.setCollision(wallID);
+        for(let i in walls){ this.groundLayer.setCollision(walls[i]); }
         this.physics.add.collider(this.guy, this.groundLayer);
             
         //* CAMERAS *//
-        let viewSize = this.ROOMSIZE * this.TILESIZE; // viewport will be confined to the square rooms with a border on the side
+        let viewSize = this.WALLSIZE * this.TILESIZE; // viewport will be confined to the square rooms with a border on the side
         this.cameras.main.setBounds(0,0,map.widthInPixels, map.heightInPixels)
             .startFollow(this.guy).stopFollow(this.guy) // ceneter cam on guy on scene load, stop follow
             .setOrigin(0).setViewport(0,0,viewSize,viewSize);
@@ -133,7 +244,7 @@ class LEVEL_1 extends Phaser.Scene {
 
         /*******     DEBUG     *******/
         // debug(drawDebug, showHTML)
-        this.debug(false, false);
+        this.debug(false, true);
         /****************************/
     }
 
